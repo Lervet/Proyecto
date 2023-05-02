dfb8fc471b58e47490eeb91ef2aa989165dc7d5b
Lervet <ericksantiago71@hotmail.com>
2023-05-02 12:42:34 -0500

    Agrego formulario login con validaciones

---
+++ Front proyecto/src/app/Services/table.service.ts
@@ -0,0 +1,12 @@
+import { Injectable } from '@angular/core';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class TableService {
+
+  constructor() { }
+  responseTable:any
+  displayedColumnsTable: string[]
+  titleTable:string;
+}
